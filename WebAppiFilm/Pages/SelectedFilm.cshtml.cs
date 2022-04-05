7d398bbf46ccbe04107b709cb8698824db778669
Andriy-sys <marchycandriy@gmail.com>
2022-04-05 21:31:41 +0300

    Ok

--- WebAppiFilm/Pages/SelectedFilm.cshtml.cs
+++ WebAppiFilm/Pages/SelectedFilm.cshtml.cs
@@ -1,6 +1,7 @@
 using FilmService.Service;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using FilmModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WebAppiFilm.Pages
 {
@@ -9,20 +10,24 @@ namespace WebAppiFilm.Pages
         private readonly IFilmService _filmService;
         private readonly ILogger<SelectedFilmModel> _logger;
         private Film _film=new();
-        public int Id { get => _film.Id; }
+        [BindProperty(SupportsGet = true)]
+        public int id { get; set; }
         public string? Name { get => _film.Name;  }
         public string? Producer { get => _film.Producer;  }
         public string? Style { get => _film.Style;  }
         public string? AboutFilm { get => _film.AboutFilm; }
         public string? Sessions { get => _film.Sessions;  }
+        
         public SelectedFilmModel(ILogger<SelectedFilmModel> logger,IFilmService filmService)
         {
             _logger = logger;
             _filmService = filmService;
-            _film = _filmService.GetFilm(Id);
+            
         }
         public void OnGet(int Id)
         {
+            id = Id;
+            _film = _filmService.GetFilm(id);
         }
     }
 }
--- WebAppiFilm/Program.cs
+++ WebAppiFilm/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
-builder.Services.AddScoped<IFilmService,FilmService.Service.FilmService>();
+builder.Services.AddFilmService();
 
 var app = builder.Build();
 
